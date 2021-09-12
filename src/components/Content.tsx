3fb2489fc240d20e601cae2781eb90c1c4823a57
Rafinhaa <rafinhaa@me.com>
2021-09-12 17:03:04 -0300

    Created the Header component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,4 @@
+import { Header } from './Header';
 import { MovieCard } from './MovieCard';
 
 interface MovieProps {
@@ -25,9 +26,7 @@ interface ContentProps {
 export function Content({movies,selectedGenre}:ContentProps) {
   return (
     <div className="container">
-      <header>
-        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-      </header>
+      <Header title={selectedGenre.title} />
       <main>
         <div className="movies-list">
           {movies.map(movie => (
